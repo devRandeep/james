4c41fca6d624d7f9c7c52e26fca53b7666132b70
reactgame <raghavrana997@gmail.com>
2023-11-21 15:34:23 +0530

    new updates

--- header.php
+++ header.php
@@ -1,19 +1,53 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- ::::::::::::::: FAVICON :::::::::::::::::::: -->
+    <link rel="icon" type="image/x-icon" href="assets/images/logo/favicon.svg">    
     <title>JAMES MOSSMAN</title>
-    <!-- /////////////// CSS -->        
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- ::::::::::::::: CSS :::::::::::::::::::: -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/sass/main.css">
-    <!-- ////////// Fonts -->
+    <!-- ::::::::::::::: FONTS ::::::::::::::::: -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100;0,9..40,200;0,9..40,300;0,9..40,400;0,9..40,500;0,9..40,600;0,9..40,700;0,9..40,800;0,9..40,900;0,9..40,1000;1,9..40,100;1,9..40,200;1,9..40,300;1,9..40,400;1,9..40,500;1,9..40,600;1,9..40,700;1,9..40,800;1,9..40,900;1,9..40,1000&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100;0,9..40,200;0,9..40,300;0,9..40,400;0,9..40,500;0,9..40,600;0,9..40,700;0,9..40,800;0,9..40,900;0,9..40,1000;1,9..40,100;1,9..40,200;1,9..40,300;1,9..40,400;1,9..40,500;1,9..40,600;1,9..40,700;1,9..40,800;1,9..40,900;1,9..40,1000&display=swap"
+        rel="stylesheet">
 
 </head>
+
 <body>
-    <h1>dfsd</h1> 
+    <!-- :::::::::::::: HEADER ::::::::::::::::::: -->
+    <header>
+        <div class="container-fluid">
+            <div class="row align-items-center">
+                <!-- ///////////// LOGO -->
+                <div class="col-md-3">
+                    <div class="header-logo">
+                        <img src="assets/images/logo/logo.svg" alt="">
+                    </div>
+                </div>
+                <!-- /////////////// MENU -->
+                <div class="col-md-9">
+                    <div class="menu-wrap">
+                        <ul>
+                            <li><a href="">Our Work</a></li>
+                            <li><a href="">Brick Library</a></li>
+                            <li><a href="">Our Clients</a></li>
+                            <li><a href="">About Us</a></li>
+                            <li><a href="">For Architects</a></li>
+                            <li><a href="">News Flash</a></li>
+                            <li><a href="" class="webBtn">Request a Call Back</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
 </body>
+
 </html>
--- index.php
+++ index.php
@@ -1,5 +1,30 @@
 <?php include 'header.php' ?>
 
+<!-- ///////////////// BANNER FRAME -->
+<section class="frameBanner">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+            <div class="bannerText">
+                <h1>Lorem ipsum dolor sit amet, consectetur.</h1>
+                <p>Lorem ipsum dolor sit amet consectetur. Dictum lobortis pellentesque a enim est est nisi. Vitae.</p>
+                <div class="contactBannerDetails align-items-center">
+                    <div class="work-btn">
+                    <a href="" class="webBtn">Our Work</a>
+                    </div>
+                    <div class="contact-details">
+                    <li><span><img src="assets/images/icons/call.svg" alt=""></span>0844 879 7405</li>
+                    <li><span><img src="assets/images/icons/mail.svg" alt=""></span>sales@jamesmossman.co.uk</li>
+                    </div>                    
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- ///////////////// BANNER FRAME CLOSE -->
+
 
 
 
