52ce1213c5806c04c96a45f51ab7064115c019f1
reactgame <raghavrana997@gmail.com>
2023-11-22 14:16:11 +0530

    new updates 22-11-2023

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- ::::::::::::::: FAVICON :::::::::::::::::::: -->
-    <link rel="icon" type="image/x-icon" href="assets/logo/favicon.svg">    
+    <link rel="icon" type="image/x-icon" href="assets/logo/favicon.svg">
     <title>JAMES MOSSMAN</title>
     <!-- ::::::::::::::: CSS :::::::::::::::::::: -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
@@ -17,7 +17,6 @@
     <link
         href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100;0,9..40,200;0,9..40,300;0,9..40,400;0,9..40,500;0,9..40,600;0,9..40,700;0,9..40,800;0,9..40,900;0,9..40,1000;1,9..40,100;1,9..40,200;1,9..40,300;1,9..40,400;1,9..40,500;1,9..40,600;1,9..40,700;1,9..40,800;1,9..40,900;1,9..40,1000&display=swap"
         rel="stylesheet">
-
 </head>
 
 <body>
--- index.php
+++ index.php
@@ -5,30 +5,159 @@
     <div class="container">
         <div class="row">
             <div class="col-md-5">
-            <div class="bannerText">
-                <h1>Lorem ipsum dolor sit amet, consectetur.</h1>
-                <p>Lorem ipsum dolor sit amet consectetur. Dictum lobortis pellentesque a enim est est nisi. Vitae.</p>
-                <div class="contactBannerDetails align-items-center">
-                    <div class="work-btn">
-                    <a href="" class="webBtn">Our Work</a>
+                <div class="bannerText">
+                    <h1>Lorem ipsum dolor sit amet, consectetur.</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur. Dictum lobortis pellentesque a enim est est nisi. Vitae.</p>
+                    <div class="contactBannerDetails align-items-center">
+                        <div class="work-btn">
+                            <a href="" class="webBtn">Our Work</a>
+                        </div>
+                        <div class="contact-details">
+                            <li><span><img src="assets/icons/call.svg" alt=""></span>0844 879 7405</li>
+                            <li><span><img src="assets/icons/mail.svg" alt=""></span>sales@jamesmossman.co.uk</li>
+                        </div>
                     </div>
-                    <div class="contact-details">
-                    <li><span><img src="assets/icons/call.svg" alt=""></span>0844 879 7405</li>
-                    <li><span><img src="assets/icons/mail.svg" alt=""></span>sales@jamesmossman.co.uk</li>
-                    </div>                    
                 </div>
-            </div>
             </div>
         </div>
     </div>
     <video width="100%$_COOKIE" height="100%" autoplay="autoplay" muted class="bannerVideo" loop>
-  <source src="assets/video/banner-video.mp4" type="video/mp4">
-  <source src="assets/video/banner-video.ogg" type="video/ogg">
-  Your browser does not support the video tag.
-</video>
+        <source src="assets/video/banner-video.mp4" type="video/mp4">
+        <source src="assets/video/banner-video.ogg" type="video/ogg">
+        Your browser does not support the video tag.
+    </video>
 </section>
-
 <!-- ///////////////// BANNER FRAME CLOSE -->
+
+
+<!-- ///////////////// HOW WE ARE -->
+<section class="howWeAre">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-md-6">
+                <div class="howweare-inbox">
+                    <h2>Who are we?</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in </p>
+                    <a href="" class="webBtn">About Us</a>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="howWeAre-image" style="background-image: url(assets/images/howweare.svg);">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- ///////////////// HOW WE ARE CLOSE -->
+
+
+
+<!-- ///////////////// BRICK LIBRARY -->
+<section class="brickLibrary">
+    <div class="container">
+        <!-- BUTTON AND TITLE ROW  -->
+        <div class="row sectionTitleAndButton">
+            <div class="col-md-6">
+                <h3>Brick Library</h3>
+            </div>
+            <div class="col-md-6 float-end">
+                <a href="" class="webBtn">View all</a>
+            </div>
+        </div>
+        <!-- BUTTON AND TITLE ROW CLOSE -->
+        <div class="row itemRow">
+            <!--  -->
+            <div class="itemBox">
+                <div class="itemImage" style="background-image: url(assets/images/wallitem1.svg);"></div>
+                <div class="itemText">
+                    <p class="itemTitle">Red Wirecut</p>
+                    <p class="itemId">JML01</p>
+                </div>
+            </div>
+             <!--  -->
+             <div class="itemBox">
+                <div class="itemImage" style="background-image: url(assets/images/wallitem2.svg);"></div>
+                <div class="itemText">
+                    <p class="itemTitle">Red Wirecut</p>
+                    <p class="itemId">JML01</p>
+                </div>
+            </div>
+             <!--  -->
+             <div class="itemBox">
+                <div class="itemImage" style="background-image: url(assets/images/wallitem3.svg);"></div>
+                <div class="itemText">
+                    <p class="itemTitle">Red Wirecut</p>
+                    <p class="itemId">JML01</p>
+                </div>
+            </div>
+             <!--  -->
+             <div class="itemBox">
+                <div class="itemImage" style="background-image: url(assets/images/wallitem4.svg);"></div>
+                <div class="itemText">
+                    <p class="itemTitle">Red Wirecut</p>
+                    <p class="itemId">JML01</p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+<!-- ///////////////// BRICK LIBRARY CLOSE -->
+
+
+
+<!-- ///////////////// OUR WORK -->
+<section class="ourWork">
+<div class="container">
+    <div class="row">
+        <!-- BUTTON AND TITLE ROW  -->
+        <div class="row sectionTitleAndButton">
+            <div class="col-md-6">
+                <h3>Our Work</h3>
+            </div>
+            <div class="col-md-6 float-end">
+                <a href="" class="webBtn">View all</a>
+            </div>
+        </div>
+        <!-- BUTTON AND TITLE ROW CLOSE -->
+    </div>
+<div class="row ourWorkRowItem">
+    <!--  -->
+    <div class="mainWorkItemBox">
+        <div class="workTextBox" style="background: linear-gradient(180deg, rgba(0, 0, 0, 0.00) 0%, rgba(0, 0, 0, 0.77) 100%), url(assets/images/work1.svg), lightgray 50% / cover no-repeat;">
+            <div class="workItemTextBox">
+            <span class="workcat">Apartments</span>
+            <h4>Lorem ipsum dolor sit amet, consectetur</h4>
+            </div>
+        </div>
+    </div>
+     <!--  -->
+     <div class="mainWorkItemBox">
+        <div class="workTextBox" style="background: linear-gradient(180deg, rgba(0, 0, 0, 0.00) 0%, rgba(0, 0, 0, 0.77) 100%), url(assets/images/work2.svg), lightgray 50% / cover no-repeat;">
+            <div class="workItemTextBox">
+            <span class="workcat">Apartments</span>
+            <h4>Lorem ipsum dolor sit amet, consectetur</h4>
+            </div>
+        </div>
+    </div>
+      <!--  -->
+      <div class="mainWorkItemBox">
+        <div class="workTextBox" style="background: linear-gradient(180deg, rgba(0, 0, 0, 0.00) 0%, rgba(0, 0, 0, 0.77) 100%), url(assets/images/work3.svg), lightgray 50% / cover no-repeat;">
+            <div class="workItemTextBox">
+            <span class="workcat">Apartments</span>
+            <h4>Lorem ipsum dolor sit amet, consectetur</h4>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+</div>
+</section>
+<!-- ///////////////// OUR WORK CLOSE -->
 
 
 
